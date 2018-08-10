56bc2175b949a95d732a896762b5525cf23eae9b
tabea <tabea@mbr-targeting.com>
2018-08-10 16:49:39 +0200

    Edit User data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :email, confirmation: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
 end
