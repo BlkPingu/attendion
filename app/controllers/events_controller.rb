6009c114281d3e2fccd3f84dbb098a4587cd6662
tabea <tabea@mbr-targeting.com>
2018-08-13 16:10:54 +0200

    Create Model for events, Create events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,12 @@
+class EventsController < ApplicationController
+  def create
+    @user = User.find(params[:user_id])
+    @event = @user.events.create(event_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :date, :time, :location, :url, :description)
+  end
+end
