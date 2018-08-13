9b6bac45d250aba35d6849829a8df6676da6e202
tabea <tabea@mbr-targeting.com>
2018-08-13 16:14:30 +0200

    Events can be deleted manually or when user is deleted

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,6 +5,13 @@ class EventsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @event = @user.events.find(params[:id])
+    @event.destroy
+    redirect_to user_path(@user)
+  end
+
   private
   def event_params
     params.require(:event).permit(:title, :date, :time, :location, :url, :description)
