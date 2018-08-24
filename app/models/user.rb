c9278f818b460765e7ced9b1bd864fb37c4289aa
tabea <tabea@mbr-targeting.com>
2018-08-24 14:08:45 +0200

    Install Devise for user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :events, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 3 }
-  validates :email, confirmation: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813134507) do
+ActiveRecord::Schema.define(version: 20180824120121) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.date "date"
-    t.time "time"
+    t.datetime "time"
     t.string "location"
     t.string "url"
     t.text "description"
@@ -26,11 +26,15 @@ ActiveRecord::Schema.define(version: 20180813134507) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
