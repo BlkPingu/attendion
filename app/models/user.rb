e65fe2e8b5f5a905cd2f35bc412f3f1f1803e8c9
tabea <tabea@mbr-targeting.com>
2018-09-04 16:22:00 +0200

    Change user profile edit to devise standard, place links accordingly

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 3 }
+ # validates :name, presence: true, length: { minimum: 3 }
   validates :email, confirmation: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
 
