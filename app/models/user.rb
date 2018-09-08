e4eabce2e753c328811e19df7227f1c265fc85ce
tabea <tabea@mbr-targeting.com>
2018-09-08 14:37:29 +0200

    Users can now successfully edit user name and about me section

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :events, dependent: :destroy
-  
+
   mount_uploader :avatar, AvatarUploader
 end
