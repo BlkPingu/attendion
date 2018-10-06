53ade41f8e0c4b15d8a344f3da6cb0f9339f988e
tabea <tabea@mbr-targeting.com>
2018-10-06 20:15:26 +0200

    Display Event Time in HH:MM

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180930163840) do
+ActiveRecord::Schema.define(version: 20181006175613) do
 
   create_table "attending_events", force: :cascade do |t|
     t.integer "event_id"
@@ -25,13 +25,13 @@ ActiveRecord::Schema.define(version: 20180930163840) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.date "date"
-    t.datetime "time"
     t.string "location"
     t.string "url"
     t.text "description"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.time "time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
