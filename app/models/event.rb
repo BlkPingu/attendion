cdbc62b5daa4588d084a1e05d6c80de92991c584
tabea <tabea@mbr-targeting.com>
2018-10-07 14:14:59 +0200

    Require events to have title and date

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ApplicationRecord
 
   has_many :attending_events, foreign_key: "user_id", dependent: :destroy #remove an events guests if the event is deleted
 
+  validates :title, presence: true
+  validates :date, presence: true
+
   # def join event
   #  attending_event.create(user_id: current_user.id, event_id: event.id)
   # end
