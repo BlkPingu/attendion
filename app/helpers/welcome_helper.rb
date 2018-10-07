bc5874bdcb4ae021fd39dc52a84f7bf7dd308e01
tabea <tabea@mbr-targeting.com>
2018-10-07 20:33:30 +0200

    Apply rubocop changes

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# Helper for Welcome Homepage
 module WelcomeHelper
 end
--- app/models/attending_event.rb
+++ app/models/attending_event.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
+# Attending Event
 class AttendingEvent < ApplicationRecord
   belongs_to :user
   belongs_to :event
 
-  validates :user_id, uniqueness: {scope: :event_id}
-
-
+  validates :user_id, uniqueness: { scope: :event_id }
 end
