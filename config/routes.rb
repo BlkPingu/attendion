eed1c01bcec25a4d0ab0b8fc29a0ca108beb5911
Tobias Kolb <tobias-kolb@hotmail.de>
2018-10-07 15:29:03 +0200

    0011/de language implementation (#53)
    * utility push
    
    * translated static sites impressum, privacy, help
    
    * added users/ index and users/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,21 @@
 Rails.application.routes.draw do
-
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 
-  get 'static_pages/help'
-
-  get 'static_pages/privacy'
-
-  get 'static_pages/impressum'
-
-  devise_for :users
-  get 'welcome/home'
-  get 'events/all', as: :events_all
-  get 'users/index'
-
+    get 'static_pages/help'
+    get 'static_pages/privacy'
+    get 'static_pages/impressum'
+    devise_for :users
+    get 'welcome/home'
+    get 'events/all', as: :events_all
+    get 'users/index'
     resources :users do
       resources :events
     end
-
     resources :events do
       member do
         get :join
       end
     end
-
     resources :events do get :join
     end
     root 'welcome#home'
