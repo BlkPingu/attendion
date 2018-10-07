05fd010a3eb178d8fd3ecfd3c568d896d6ec3b61
BlkPingu <tobias-kolb@hotmail.de>
2018-10-07 14:33:13 +0200

    translated static sites impressum, privacy, help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :aboutme, :password, :password_confirmation, :avatar, :current_password])
   end
 
+  private
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options(options = {})
     { locale: I18n.locale }.merge options
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,23 @@
 Rails.application.routes.draw do
-
-  scope "(:locale)", locale: /en|de/ do
-
-    root 'welcome#home'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 
     get 'static_pages/help'
     get 'static_pages/privacy'
     get 'static_pages/impressum'
-
     devise_for :users
     get 'welcome/home'
     get 'events/all', as: :events_all
     get 'users/index'
-
     resources :users do
       resources :events
     end
-
     resources :events do
       member do
         get :join
       end
     end
-
-    resources :events do
-      get :join
+    resources :events do get :join
     end
+    root 'welcome#home'
   end
 end
