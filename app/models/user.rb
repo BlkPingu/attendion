a8a892a9e47c04b181590282765574a0b8e9f5b2
BlkPingu <tobias-kolb@hotmail.de>
2018-10-07 20:23:28 +0200

    if user gets deleted, the event gets deleted, the postion as guest gets deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, dependent: :destroy
-  has_many :attending_events, dependent: :destroy
+  has_many :events, :dependent => :destroy
+  has_many :attending_events, :dependent  => :destroy
 
   mount_uploader :avatar, AvatarUploader
 
