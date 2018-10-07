96d8043fce2223d34a8b81280419772b8ea73d58
tabea <tabea@mbr-targeting.com>
2018-10-07 19:35:39 +0200

    Add bullet to testing settings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,27 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+module MiniTestWithBullet
+  require 'minitest/unit'
+
+  def before_setup
+    Bullet.start_request
+    super if defined?(super)
+  end
+
+  def after_teardown
+    super if defined?(super)
+    Bullet.perform_out_of_channel_notifications if Bullet.notification?
+    Bullet.end_request
+  end
+end
+
+module ActiveSupport
+  class TestCase
+    include MiniTestWithBullet
+  end
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
