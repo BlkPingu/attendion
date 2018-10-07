3ff8dd013ed9ea5e7a985bc46963aec0de0bfb3c
tabea <tabea@mbr-targeting.com>
2018-10-07 23:45:16 +0200

    Remove doubled log in message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = current_user
 
     if @user.update(user_params)
       redirect_to @user
