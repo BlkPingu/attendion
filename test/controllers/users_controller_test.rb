eaccd48ef8960b292b18afea676154eb3efc89d5
tabea <tabea@mbr-targeting.com>
2018-10-08 13:51:59 +0200

    Happy testing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,10 +3,25 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'authenticated users can see all users' do
-    sign_in users()
+  test "Users who aren't logged in cannot see users" do
+    get users_index_path
+    assert_response :redirect
+  end
 
-    get '/users'
+  test "Users who aren't logged in cannot see events" do
+    get events_all_path
+    assert_response :redirect
+  end
+
+  test "Everyone can get to User Registration" do
+    get new_user_registration_path
     assert_response :success
   end
+
+  test "Registered users can see all Users" do
+    sign_in users(:one)
+    get users_index_path
+    assert_response :success
+  end
+
 end
